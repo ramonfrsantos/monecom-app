25bd0639ae81183074dc86cc138d82bec052016b
Ramon Santos <ramonsantos@frwk.com.br>
2020-11-24 19:11:23 -0300

    Arrumando mensagens de monitoramento

--- lib/components/email_button_component.dart
+++ lib/components/email_button_component.dart
@@ -1,15 +1,19 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:monecom/library/models/mysql.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 DocumentSnapshot snapshot;
 
 // ignore: must_be_immutable
 class EmailButton extends StatelessWidget {
+  // instanciando banco mysql
+  var db = Mysql();
   var area;
-  int idSensor;
+  var idSensor;
   var data;
 
+  List<String> body = [];
   EmailButton(this.area, this.idSensor, this.data);
 
   @override
@@ -46,18 +50,39 @@ class EmailButton extends StatelessWidget {
 
                   String listaEmails = removeLastChar(mails);
 
-                  final Uri _emailLaunchUri = Uri(
-                    scheme: 'mailto',
-                    path: '$listaEmails',
-                    queryParameters: {
-                      'subject':
-                          '[Mon&Com] Estamos enviando informações do monitoramento.',
-                      'body':
-                          "O sensor $idSensor foi acionado na área $area, em ${data.toString().substring(8, 10).toLowerCase()}/${data.toString().substring(5, 7)}/${data.toString().substring(0, 4)}, às ${data.toString().substring(11, 19)}."
-                    },
-                  );
+                  db.getConnection().then((conn) {
+                    String meusIds =
+                        'select idSensor,data,area from registroIot_V2;';
+                    conn.query(meusIds).then((results) {
+                      var meusResults = results.toList();
 
-                  launch(_emailLaunchUri.toString());
+                      for (int i = 0; i < meusResults.length; i++) {
+                        String idSensor =
+                            meusResults.elementAt(i).values[0].toString();
+                        String data =
+                            meusResults.elementAt(i).values[1].toString();
+                        String area =
+                            meusResults.elementAt(i).values[2].toString();
+
+                        String aux =
+                            "O sensor $idSensor foi acionado na área $area;\nEm ${data.toString().substring(8, 10).toLowerCase()}/${data.toString().substring(5, 7)}/${data.toString().substring(0, 4)};\nÀs ${data.toString().substring(11, 19)};\n\n";
+
+                        body.add(aux);
+                      }
+
+                      final Uri _emailLaunchUri = Uri(
+                        scheme: 'mailto',
+                        path: '$listaEmails',
+                        queryParameters: {
+                          'subject':
+                              '[Mon&Com] Estamos enviando informações do monitoramento.',
+                          'body':
+                              '${body.toString().replaceAll('[', '').replaceAll(']', '').replaceAll(',', '')}'
+                        },
+                      );
+                      launch(_emailLaunchUri.toString());
+                    });
+                  });
                 });
               })),
     );
